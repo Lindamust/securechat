f81f03569f54f43277e4803731e6d42ba5bdf14e
Lindamust <208328889+Lindamust@users.noreply.github.com>
2026-05-23 11:44:51 +1000

    removed unnecessary manual validation macro

--- server/validation-core/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod error;
-pub mod validate;
-
-pub use error::*;
-pub use validate::*;
--- server/validation-core/src/validate.rs
+++
@@ -1,6 +0,0 @@
-pub trait ValidateObject {
-    type Output;
-    type Error;
-
-    fn try_validate(self) -> Result<Self::Output, Self::Error>;
-}
