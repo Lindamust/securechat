3a8fb75ba19db622556da9a6e2164e6bff1ef7d2
Lindamust <208328889+Lindamust@users.noreply.github.com>
2026-06-05 11:51:06 +1000

    change Step's return to be bounded by a generic NewCtx on the method instead of an amalgamation of Sculptor Remainings

--- server/crates/pipeline-core/src/step.rs
+++ server/crates/pipeline-core/src/step.rs
@@ -1,6 +1,5 @@
 use frunk::{
-    HCons,
-    hlist::{HList, Sculptor},
+    hlist::{HList, Sculptor}
 };
 
 use std::future::Future;
@@ -10,82 +9,49 @@ use crate::error::PipelineResult;
 pub trait ExecutorFor<S: ?Sized> {}
 
 pub trait PureStep {
-    type Needs: HList;
-    type Provides: HList;
+    type Needs;
+    type Provides;
 
-    type Remainder<H, Idx>
+    fn run<OldCtx, NewCtx, Idx>(self, ctx: OldCtx) -> PipelineResult<NewCtx>
     where
-        H: Sculptor<Self::Needs, Idx> + HList;
-
-    fn run<H, Idx>(self, ctx: H) -> PipelineResult<HCons<Self::Provides, Self::Remainder<H, Idx>>>
-    where
-        H: Sculptor<Self::Needs, Idx> + HList;
+        OldCtx: Sculptor<Self::Needs, Idx>,
+        NewCtx: HList;
 }
 
-// pub trait PureConsumingStep {
-//     type Needs: HList;
-//     type Provides: HList;
-
-//     fn run<H, Idx, Rem>(self, ctx: H) -> PipelineResult<HCons<Self::Provides, Rem>>
-//     where
-//         H: Sculptor<Self::Needs, Idx, Remainder = Rem>;
-// }
-
 pub trait AsyncStep {
-    type Needs: HList;
-    type Provides: HList;
+    type Needs;
+    type Provides;
 
-    type Remainder<H, Idx>
-    where
-        H: Sculptor<Self::Needs, Idx> + HList;
-
-    fn run<H, Idx, Exec>(
+    fn run<OldCtx, NewCtx, Idx, Exec>(
         self,
-        ctx: H,
+        ctx: OldCtx,
         executor: &Exec,
-    ) -> impl Future<Output = PipelineResult<HCons<Self::Provides, Self::Remainder<H, Idx>>>> + Send
+    ) -> impl Future<Output = PipelineResult<NewCtx>> + Send
     where
-        H: Sculptor<Self::Needs, Idx> + HList +Send,
-        Self::Remainder<H, Idx>: Send,
+        OldCtx: Sculptor<Self::Needs, Idx> + Send,
+        NewCtx: HList + Send,
         Exec: ExecutorFor<Self> + ?Sized + Sync;
 }
 
-// pub trait AsyncConsumingStep {
-//     type Needs: HList;
-//     type Provides: HList;
-
-//     fn run<H, Idx, Exec, Rem>(
-//         self,
-//         ctx: H,
-//         executor: &Exec,
-//     ) -> impl Future<Output = PipelineResult<HCons<Self::Provides, Rem>>> + Send
-//     where
-//         H: Sculptor<Self::Needs, Idx, Remainder = Rem> + Send,
-//         Exec: ExecutorFor<Self> + ?Sized + Sync;
-// }
-
 impl<Exec, S: PureStep> ExecutorFor<S> for Exec {}
 
-impl<S: PureStep + Send> AsyncStep for S {
-    type Needs = <S as PureStep>::Needs;
-    type Provides = <S as PureStep>::Provides;
+impl<S: PureStep + Send> AsyncStep for S
+{
+    type Needs = S::Needs;
+    type Provides = S::Provides;
 
-    type Remainder<H, Idx> = <S as PureStep>::Remainder<H, Idx>
-    where
-        H: Sculptor<Self::Needs, Idx> + HList;
-
-    fn run<H, Idx, Exec>(
+    fn run<OldCtx, NewCtx, Idx, Exec>(
         self,
-        ctx: H,
+        ctx: OldCtx,
         _executor: &Exec,
-    ) -> impl Future<Output = PipelineResult<HCons<Self::Provides, Self::Remainder<H, Idx>>>> + Send
+    ) -> impl Future<Output = PipelineResult<NewCtx>> + Send
     where
-        H: Sculptor<<S as PureStep>::Needs, Idx> + HList + Send,
-        Self::Remainder<H, Idx>: Send,
-        Exec: ExecutorFor<S> + ?Sized + Sync,
+        OldCtx: Sculptor<Self::Needs, Idx> + Send,
+        NewCtx: HList + Send,
+        Exec: ExecutorFor<Self> + ?Sized + Sync
     {
-        async move { PureStep::run(self, ctx) }
+        async move {
+            PureStep::run(self, ctx)
+        }
     }
 }
-
-// impl<Exec, S: PureConsumingStep> ExecutorFor<S> for Exec
