21890c654fed60f68eb427cee63ac285323c4555
Lindamust <208328889+Lindamust@users.noreply.github.com>
2026-06-11 11:14:32 +1000

    Update user.rs

--- server/crates/app/src/steps/user.rs
+++ server/crates/app/src/steps/user.rs
@@ -12,17 +12,17 @@ impl PureStep for HashPassword {
     type Needs = HList![RegisterBody];
     type Provides = NewUser;
 
-    type Output<Ctx> = HCons<NewUser, <Ctx as Plucker<RegisterBody, _>>::Remainder>
-        where
-            Ctx: HList;
-
-    fn run<Ctx, Idx>(self, ctx: Ctx) -> pipeline_core::error::PipelineResult<Self::Output<Ctx>>
-        where
-            Ctx: Sculptor<Self::Needs, Idx> + HList,
+    fn run_pure<Ctx, Idx>(
+        self,
+        ctx: Ctx,
+    ) -> PipelineResult<<Ctx::Remainder as Prepends<Self::Provides>>::Output>
+    where
+        Ctx: HList + Sculptor<Self::Target, Idx>,
+        Ctx::Remainder: HList + Prepends<Self::Provides>,
+        <Ctx::Remainder as Prepends<Self::Provides>>::Output: HList
     {
-        let (hlist_pat![a], r) = ctx.sculpt();
-
-        let hashed_password = a.password.hash();
+        let (hlist_pat![reg_body], rem] = ctx.sculpt();
+        let hashed_password = reg_body.password.hash();
 
         let new_user = NewUser {
             username: a.username,
@@ -35,37 +35,49 @@ impl PureStep for HashPassword {
             otpks: a.otpks,
         };
 
-        Ok(hlist_macro![new_user, r])
+        Ok(rem.prepend_type(new_user))
     }
 }
 
 #[derive(Clone)]
 pub struct StoreUser;
 
+pub trait PureStep {
+    type Needs: HList;
+    type Provides;
+
+    fn run_pure<Ctx, Idx>(
+        self,
+        ctx: Ctx,
+    ) -> PipelineResult<<Ctx::Remainder as Prepends<Self::Provides>>::Output>
+    where
+        Ctx: HList + Sculptor<Self::Target, Idx>,
+        Ctx::Remainder: HList + Prepends<Self::Provides>,
+        <Ctx::Remainder as Prepends<Self::Provides>>::Output: HList;
+}
+
 impl AsyncStep for StoreUser {
     type Needs = HList![NewUser];
-    type Provides = HList![InsertedUser];
+    type Provides = InsertedUser;
 
-    type Output<Ctx> = HCons<InsertedUser, Ctx>
-        where
-            Ctx: HList;
-
-    fn run<Ctx, Idx, Exec>(
+    fn run_async<Ctx, Exec, Idx>(
         self,
         ctx: Ctx,
         executor: &Exec,
-    ) -> impl Future<Output = pipeline_core::error::PipelineResult<Self::Output<Ctx>>> + Send
+    ) -> impl Future<Output = PipelineResult<<Ctx::Remainder as Prepends<Self::Provides>>::Output>> + Send
     where
-        Ctx: Sculptor<Self::Needs, Idx> + HList + Send,
-        Exec: pipeline_core::step::ExecutorFor<Self> + ?Sized + Sync
+        Ctx: HList + Sculptor<Self::Target, Idx> + Send,
+        Ctx::Remainder: HList + Prepends<Self::Provides> + Send,
+        <Ctx::Remainder as Prepends<Self::Provides>>::Output: HList + Send,
+        Exec: ExecutorFor<Self> + ?Sized + Sync,
     {
         async move {
-            let (hlist_pat![new_user], remainder) = ctx.sculpt();
+            let (hlist_pat![new_user], rem) = ctx.sculpt();
 
             let pg = unsafe { &*(executor as *const Exec as *const PgDatabase)  };
             let res = pg.insert_user(&new_user).await?;
 
-            Ok(HCons { head: hlist_macro![res], tail: remainder })
+            Ok(rem.prepend_type(res))
         }
     }
 }
