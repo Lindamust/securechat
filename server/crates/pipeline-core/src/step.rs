4ece698ae32c84d882388d98b2a9a30b892842f5
Lindamust <208328889+Lindamust@users.noreply.github.com>
2026-06-11 10:59:04 +1000

    Update step.rs

--- server/crates/pipeline-core/src/step.rs
+++ server/crates/pipeline-core/src/step.rs
@@ -2,88 +2,58 @@ use core::ops::Add;
 
 use frunk::hlist::{HList, Sculptor};
 
-use crate::{error::PipelineResult, hlist::ExtendsWith};
+use crate::{error::PipelineResult, hlist::Prepends};
 
 pub trait ExecutorFor<S: ?Sized> {}
 
-pub trait SculptRem {
-    type Target: HList;
-    type Extends: HList;
-
-    type Rem<Ctx, Idx>: HList + ExtendsWith<Self::Target>
-    where
-        Ctx: HList + Sculptor<Self::Target, Idx>,
-        Ctx::Remainder: HList + ExtendsWith<Self::Extends>;
-}
-
-pub trait DeriveNewCtx<Ctx, Idx>: SculptRem {
-    type NewDerivedCtx: HList;
-}
-
-impl<T, Ctx, Idx> DeriveNewCtx<Ctx, Idx> for T
-where
-    T: SculptRem,
-    T::Rem<Ctx, Idx>: HList,
-    Ctx: HList + Sculptor<T::Target, Idx>,
-    Ctx::Remainder: HList + ExtendsWith<T::Extends>,
-{
-    type NewDerivedCtx = <T::Rem<Ctx, Idx> as ExtendsWith<Self::Target>>::Output;
-}
-
-pub trait PureStep: SculptRem {
+pub trait PureStep {
     type Needs: HList;
-    type Provides: HList;
+    type Provides;
 
     fn run_pure<Ctx, Idx>(
         self,
         ctx: Ctx,
-    ) -> PipelineResult<<Self as DeriveNewCtx<Ctx, Idx>>::NewDerivedCtx>
+    ) -> PipelineResult<<Ctx::Remainder as Prepends<Self::Provides>>::Output>
     where
-        Self: Sized,
         Ctx: HList + Sculptor<Self::Target, Idx>,
-        Ctx::Remainder: HList + Add<Self::Extends>,
-        <Ctx::Remainder as Add<Self::Extends>>::Output: HList;
+        Ctx::Remainder: HList + Prepends<Self::Provides>,
+        <Ctx::Remainder as Prepends<Self::Provides>>::Output: HList;
 }
 
 impl<Exec, S: PureStep> ExecutorFor<S> for Exec {}
 
-pub trait AsyncStep: SculptRem {
+pub trait AsyncStep {
     type Needs: HList;
-    type Provides: HList;
+    type Provides;
 
-    fn run_async<Ctx, Exec, Idx>(
+    fn run_pure<Ctx, Exec, Idx>(
         self,
         ctx: Ctx,
         executor: &Exec,
-    ) -> impl Future<Output = PipelineResult<<Self as DeriveNewCtx<Ctx, Idx>>::NewDerivedCtx>> + Send
+    ) -> impl Future<Output = PipelineResult<<Ctx::Remainder as Prepends<Self::Provides>>::Output>> + Send
     where
-        Self: Sized + Send,
-        Self::Rem<Ctx, Idx>: Send,
-        <Self::Rem<Ctx, Idx> as ExtendsWith<Self::Target>>::Output: Send,
         Ctx: HList + Sculptor<Self::Target, Idx> + Send,
-        Ctx::Remainder: HList + Add<Self::Extends> + Send,
-        <Ctx::Remainder as Add<Self::Extends>>::Output: HList + Send,
-        Exec: ?Sized + Sync;
+        Ctx::Remainder: HList + Prepends<Self::Provides> + Send,
+        <Ctx::Remainder as Prepends<Self::Provides>>::Output: HList + Send,
+        Exec: ?Sized + Sync + ExecutorFor<Self>;
 }
 
 impl<S: PureStep + Send> AsyncStep for S {
-    type Needs = S::Needs;
-    type Provides = S::Provides;
+    type Needs = <S as PureStep>::Needs;
+    type Provides = <S as PureStep>::Provides;
 
-    fn run_async<Ctx, Exec, Idx>(
+    fn run_pure<Ctx, Exec, Idx>(
         self,
         ctx: Ctx,
         _executor: &Exec,
-    ) -> impl Future<Output = PipelineResult<<Self as DeriveNewCtx<Ctx, Idx>>::NewDerivedCtx>> + Send
+    ) -> impl Future<Output = PipelineResult<<Ctx::Remainder as Prepends<Self::Provides>>::Output>> + Send
     where
-        Self: Sized + Send,
-        Self::Rem<Ctx, Idx>: Send,
-        <Self::Rem<Ctx, Idx> as ExtendsWith<Self::Target>>::Output: Send,
         Ctx: HList + Sculptor<Self::Target, Idx> + Send,
-        Ctx::Remainder: HList + Add<Self::Extends> + Send,
-        <Ctx::Remainder as Add<Self::Extends>>::Output: HList + Send,
-        Exec: ?Sized + Sync,
+        Ctx::Remainder: HList + Prepends<Self::Provides> + Send,
+        <Ctx::Remainder as Prepends<Self::Provides>>::Output: HList + Send,
+        Exec: ?Sized + Sync + ExecutorFor<Self> 
     {
-        core::future::ready(PureStep::run_pure(self, ctx))
+        core::future::ready(PureStep::run(self, ctx))
     }
 }
+
